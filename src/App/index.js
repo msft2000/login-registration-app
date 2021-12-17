614fb332f642957b207825d802d662fe8c0f63a1
Mateo Flores <dragonnegro1231@gmail.com>
2021-12-17 11:31:22 -0500

    Solucion de problemas con react-router

--- src/App/index.js
+++ src/App/index.js
@@ -7,6 +7,7 @@ import { Routes,Route,useNavigate } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import {register_login,googleLogin} from './fireBase-authentication';
+
 function App() {
   const [email, setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
@@ -20,52 +21,52 @@ function App() {
   return (
       <div className='App'>
         <>
-          <Routes>
-            <Route 
-              path="/" 
-              element={<Form 
-                        title="Login" 
-                        otherTitle="or Register"
-                        changeOption={()=>navigate('/register')}
-                        setEmail={ setEmail }
-                        setPassword={ setPassword } 
-                        handleAction={ ()=>register_login(1,email,password,toast,navigate) }
-                      />}
-              
-            />
-            <Route 
-              path="/login" 
-              element={<Form 
-                        title="Login" 
-                        otherTitle="or Register"
-                        changeOption={()=>navigate('/register')}
-                        setEmail={ setEmail }
-                        setPassword={ setPassword } 
-                        handleAction={ ()=>register_login(1,email,password,toast,navigate) }
-                        googleLogin={()=>googleLogin(toast,navigate)}
-                      />}
-              
-            />
-            <Route 
-              path="/register" 
-              element={<Form 
-                          title="Register"
-                          otherTitle="or Login"
-                          changeOption={()=>navigate('/login')}
+            <Routes>
+              <Route 
+                path="/" 
+                element={<Form 
+                          title="Login" 
+                          otherTitle="or Register"
+                          changeOption={()=>navigate('/register')}
                           setEmail={ setEmail }
-                          setPassword={ setPassword }
-                          handleAction={ ()=>register_login(2,email,password,toast,navigate) }
+                          setPassword={ setPassword } 
+                          handleAction={ ()=>register_login(1,email,password,toast,navigate) }
+                        />}
+                
+              />
+              <Route 
+                path="/login" 
+                element={<Form 
+                          title="Login" 
+                          otherTitle="or Register"
+                          changeOption={()=>navigate('/register')}
+                          setEmail={ setEmail }
+                          setPassword={ setPassword } 
+                          handleAction={ ()=>register_login(1,email,password,toast,navigate) }
                           googleLogin={()=>googleLogin(toast,navigate)}
                         />}
-              
-            />
-            <Route
-              path='/home'
-              element={
-                <Home />
-              }
-            />
-          </Routes>
+                
+              />
+              <Route 
+                path="/register" 
+                element={<Form 
+                            title="Register"
+                            otherTitle="or Login"
+                            changeOption={()=>navigate('/login')}
+                            setEmail={ setEmail }
+                            setPassword={ setPassword }
+                            handleAction={ ()=>register_login(2,email,password,toast,navigate) }
+                            googleLogin={()=>googleLogin(toast,navigate)}
+                          />}
+                
+              />
+              <Route
+                path='/home'
+                element={
+                  <Home />
+                }
+              />
+            </Routes>
         </>
         <ToastContainer />
         {/* <img className="App-logo" alt="Logo Spin" src={ logo } /> */}
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
+
 ReactDOM.render(
   <React.StrictMode>
     <Router>

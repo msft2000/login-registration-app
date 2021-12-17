e7945e3755cb1cc0947e73f5393855fb98022f7c
Mateo Flores <dragonnegro1231@gmail.com>
2021-12-17 11:24:31 -0500

    Solucion de problemas con react-router

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { useNavigate } from 'react-router-dom';
 import Button from '@mui/material/Button';
 import DeleteIcon from '@mui/icons-material/Delete';
 import '../css/Home.css';
 export default function Home(){
     const navigate=useNavigate();
-    React.useEffect(()=>{
+    useEffect(()=>{
         let authToken = sessionStorage.getItem('Auth Token');
         if(authToken){
             navigate('/home');
@@ -19,6 +19,7 @@ export default function Home(){
     }
     return(
         <div id="home">
+            {/* {validateUser()} */}
             <h1>Home Page</h1>
             <Button onClick={handleLogOut} variant="outlined" startIcon={<DeleteIcon />}>
                 Log Out
